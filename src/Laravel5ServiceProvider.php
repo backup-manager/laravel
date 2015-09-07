2917347ffdb991f545c85e91efaef4139ef0284a
Burak Çalık <burak@macellan.net>
2015-09-07 14:55:02 +0300

    Added Google Cloud Storage support.

--- src/Laravel5ServiceProvider.php
+++ src/Laravel5ServiceProvider.php
@@ -50,6 +50,7 @@ class Laravel5ServiceProvider extends ServiceProvider {
         $this->app->bind(\BackupManager\Filesystems\FilesystemProvider::class, function ($app) {
             $provider = new Filesystems\FilesystemProvider(new Config($app['config']['backup-manager']));
             $provider->add(new Filesystems\Awss3Filesystem);
+            $provider->add(new Filesystems\GcsFilesystem);
             $provider->add(new Filesystems\DropboxFilesystem);
             $provider->add(new Filesystems\FtpFilesystem);
             $provider->add(new Filesystems\LocalFilesystem);
